aad8a72495ba9af6fe8973c20dbce849715a3f59
satvk <satvikkatoch@gmail.com>
2024-07-21 14:21:27 +0530

    minor changes

--- schemaTypes/profile.ts
+++ schemaTypes/profile.ts
@@ -82,16 +82,10 @@ const profile = {
           initialValue: "https://linkedin.com/in/",
         },
         {
-          name: "twitter",
-          title: "Twitter URL",
+          name: "x",
+          title: "X URL",
           type: "url",
           initialValue: "https://twitter.com/",
-        },
-        {
-          name: "twitch",
-          title: "Twitch URL",
-          type: "url",
-          initialValue: "https://twitch.com/",
         },
       ],
       options: {
