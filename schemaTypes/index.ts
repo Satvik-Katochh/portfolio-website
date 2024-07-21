09d6eb2c24be7bbecc5c958683df2c8a582fc72b
satvk <satvikkatoch@gmail.com>
2024-07-21 18:54:27 +0530

    nedd to start on adding projects

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,5 +1,5 @@
-// schemas/index.ts
-
+import job from "./job";
 import profile from "./profile";
+import project from "./project";
 
-export const schemaTypes = [profile];
+export const schemaTypes = [profile, job, project];
